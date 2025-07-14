c4867b26be2c9ccce94165ab7d1feae4e74e933f
desan <desanctisisabella95@gmail.com>
2025-07-14 21:17:00 +0200

    Student class with StudentRepository class

--- src/main/java/com/develhope/crudTestDemo/model/Student.java
+++ src/main/java/com/develhope/crudTestDemo/model/Student.java
@@ -1,4 +1,27 @@
 package com.develhope.crudTestDemo.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(length= 100, nullable = false)
+    private String name;
+    @Column(length= 100,nullable = false)
+    private String surname;
+    @Column(nullable = false)
+    private boolean isWorking;
+
+    public Student() {
+
+    }
 }
--- src/main/java/com/develhope/crudTestDemo/repository/StudentRepository.java
+++ src/main/java/com/develhope/crudTestDemo/repository/StudentRepository.java
@@ -1,4 +1,9 @@
 package com.develhope.crudTestDemo.repository;
 
-public class StudentRepository {
+import com.develhope.crudTestDemo.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends  JpaRepository<Student,Integer> {
 }
